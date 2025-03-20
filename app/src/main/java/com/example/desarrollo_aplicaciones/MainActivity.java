dd189aaae0d2058644fdff8ed9a767b4d0bec814
Edgardo Crespo Lindeman <edgcrespo@uade.edu.ar>
2025-03-20 20:53:26 -0300

    Login y Register // Auth

--- app/src/main/java/com/example/desarrollo_aplicaciones/MainActivity.java
+++ app/src/main/java/com/example/desarrollo_aplicaciones/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.desarrollo_aplicaciones;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.desarrollo_aplicaciones.LogReg.LoginActivity;
+import com.example.desarrollo_aplicaciones.LogReg.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button registerButton = findViewById(R.id.registerButton);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        registerButton.setOnClickListener(v -> {
+            Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(registerIntent);
+        });
+
+        loginButton.setOnClickListener(v -> {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
         });
     }
 }
